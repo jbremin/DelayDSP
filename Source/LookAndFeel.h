45e565716cfaa1159437de90902073d90ff36ad3
Johan Bremin <jbremin@hotmail.com>
2024-07-24 16:21:54 +0200

    Additional UI tweaks for the text box and caret.

--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -70,6 +70,13 @@ public:
     void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
                           float sliderPos, float rotaryStartAngle,
                           float rotaryEndAngle, juce::Slider& slider) override;
+    
+    juce::Label* createSliderTextBox(juce::Slider&) override;
+    
+    void drawTextEditorOutline(juce::Graphics&, int, int, juce::TextEditor&) override { }
+    
+    void fillTextEditorBackground(juce::Graphics&, int width, int height, juce::TextEditor&) override;
+    
 private:
     juce::DropShadow dropShadow { Colors::Knob::dropShadow, 6, { 0, 3 } };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RotaryKnobLookAndFeel)
